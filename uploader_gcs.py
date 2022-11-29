5e8c93e996ebbc54291f7036b2973b40a6cd89b6
zeeniye <bluemul07@gmail.com>
2022-11-29 14:54:58 +0800

    python script complete

--- uploader_gcs.py
+++ uploader_gcs.py
@@ -1,13 +1,13 @@
-import os
 import sys
+import time
 import argparse
+import urllib.request as rqlib
 from google.cloud import storage
 
-def authenticate_implicit_with_adc(project_id="your-google-cloud-project-id"):
+def upload_img_gcs(GCP_PROJECT, GCP_BUCKET):
     """
     The library can auto-detect the credentials of Google Cloud Client to use.
     Only if:
-
     * Before running this sample, run in terminal `gcloud auth application-default login`
     as described in https://cloud.google.com/docs/authentication/external/set-up-adc
     * To ensure, the credentials (default) will available on:\n
@@ -15,24 +15,62 @@ def authenticate_implicit_with_adc(project_id="your-google-cloud-project-id"):
     `Windows: %APPDATA%/gcloud/application_default_credentials.json`
     https://cloud.google.com/docs/authentication/application-default-credentials#personal
     """
-    storage_client = storage.Client(project=project_id)
-    buckets = storage_client.list_buckets()
-    print("Buckets:")
-    for bucket in buckets:
-        print(bucket.name)
-    print("Listed all storage buckets.")
+
+    storage_client = storage.Client(project=GCP_PROJECT)
+
+    buckets_list = storage_client.list_buckets() # list all bucket
+    bck_ls = [bc.name for bc in buckets_list if (bc.name in GCP_BUCKET)] # get all bucket name
+    # res = [ele for ele in test_list if(ele in test_string)]
+
+    if not bool(bck_ls):
+        bucket = storage_client.create_bucket(GCP_BUCKET) # make new bucket
+    
+    bucket = storage_client.get_bucket(GCP_BUCKET) # set to the current bucket
+    return bucket
 
 def args_p():
-    parser = argparse.ArgumentParser(description='input for gcp_project_id, gcp_bucket_name, and image_url')
-    parser.add_argument('project_id', type=str, help='Type in your GCP project id')
+    """Argument to pass into terminal/script"""
+    parser = argparse.ArgumentParser(description='> Image Uploader to Google Cloud Storage <')
+    parser.add_argument('project_id',type=str,
+    metavar='PROJECT_GCP',
+    help='Type in your GCP project id (existed)')
+        
+    parser.add_argument('bucket_name',type=str,
+    metavar='BUCKET_GCP',
+    help='Type your GCP bucket name (existed or create new)')
+
+    parser.add_argument('-i', '--img_url',type=str,
+    metavar='IMAGE_URL',
+    help='Type your online image URL',
+    default='https://images.unsplash.com/photo-1553550319-d8d5393e1c80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c25vdyUyMGhvdXNlfGVufDB8fDB8fA%3D%3D&w=1000&q=80.jpg')
+
+    t = time.localtime()
+    current_time = str(time.strftime("%y%m%d%H%M%S", t)) # now time yymmddHHMMSS
+
+    parser.add_argument('-n', '--img_name', type=str,
+    metavar='IMAGE_NAME',
+    help='Type your online image name for uploaded to the GCS',
+    default=f'snow_house_{current_time}.jpg')
+
     args = parser.parse_args()
     return args
 
+def get_img_online(URL_IMG):
+    """Retrieved and opened image from the internet by URL"""
+    file = rqlib.urlopen(URL_IMG)
+    return file
+
 if __name__ == '__main__':
     try:
-        authenticate_implicit_with_adc(args_p().project_id)
-        print('image uploaded')
+        prgs = args_p()
+
+        img_file = get_img_online(prgs.img_url)
+        buckt = upload_img_gcs(prgs.project_id, prgs.bucket_name)
+        blob = buckt.blob(prgs.img_name)
+        blob.upload_from_string(img_file.read(), content_type='image/jpg')
+
+        print('image uploaded v')
     except:
-        print('failed!!!')
+        print('failed x')
     finally:
         print('~ thank you for using this script ~')
