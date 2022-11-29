70d8cc5977a647adfad0912512eaa5d2ed68b0ce
zeeniye <bluemul07@gmail.com>
2022-11-29 22:34:58 +0800

    change to auth

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,23 @@
 # Deriving the base image (~45MB)
 FROM python:3.8-slim-bullseye
 
-WORKDIR /usr/src/app
+WORKDIR /my-uploader
 
-RUN python3 -m venv /envgcs
+# RUN python3 -m venv /envgcs
 
 # Installing dependencies
 COPY requirements.txt .
-RUN . /envgcs/bin/activate && pip install --no-cache-dir -r requirements.txt
+# RUN . /envgcs/bin/activate && pip install --upgrade pip 
+# RUN . /envgcs/bin/activate && pip install -r requirements.txt
+
+# If not using virtual environment
+RUN pip install --upgrade pip 
+RUN pip install -r requirements.txt
 
 # Copy the rest of files
-COPY . .
+COPY uploader_gcs.py .
 
 # Execute the script
-CMD ["/envgcs/bin/activate"]
+# CMD ["/envgcs/bin/activate"]
 ENTRYPOINT ["python", "uploader_gcs.py"]
+# CMD ["python", "uploader_gcs.py", "(PROJECT_GCP)", "(BUCKET_GCP)", "-i", "(IMAGE_URL)", "-n", "IMAGE_NAME"]
--- uploader_gcs.py
+++ uploader_gcs.py
@@ -1,10 +1,11 @@
 import sys
 import time
 import argparse
+import google.auth
 import urllib.request as rqlib
 from google.cloud import storage
 
-def upload_img_gcs(GCP_PROJECT, GCP_BUCKET):
+def upload_img_gcs(GCP_BUCKET, GCP_PROJECT='x'):
     """
     The library can auto-detect the credentials of Google Cloud Client to use.
     Only if:
@@ -14,9 +15,11 @@ def upload_img_gcs(GCP_PROJECT, GCP_BUCKET):
     `Linux, macOS: $HOME/.config/gcloud/application_default_credentials.json`;
     `Windows: %APPDATA%/gcloud/application_default_credentials.json`
     https://cloud.google.com/docs/authentication/application-default-credentials#personal
+    * For the credentials file (.json) need to copy inside this file.
     """
 
-    storage_client = storage.Client(project=GCP_PROJECT)
+    credentials, project_id = google.auth.default()
+    storage_client = storage.Client(project=project_id)
 
     buckets_list = storage_client.list_buckets() # list all bucket
     bck_ls = [bc.name for bc in buckets_list if (bc.name in GCP_BUCKET)] # get all bucket name
@@ -31,9 +34,9 @@ def upload_img_gcs(GCP_PROJECT, GCP_BUCKET):
 def args_p():
     """Argument to pass into terminal/script"""
     parser = argparse.ArgumentParser(description='> Image Uploader to Google Cloud Storage <')
-    parser.add_argument('project_id',type=str,
-    metavar='PROJECT_GCP',
-    help='Type in your GCP project id (existed)')
+    # parser.add_argument('project_id',type=str,
+    # metavar='PROJECT_GCP',
+    # help='Type in your GCP project id (existed)')
         
     parser.add_argument('bucket_name',type=str,
     metavar='BUCKET_GCP',
@@ -65,7 +68,7 @@ if __name__ == '__main__':
         prgs = args_p()
 
         img_file = get_img_online(prgs.img_url)
-        buckt = upload_img_gcs(prgs.project_id, prgs.bucket_name)
+        buckt = upload_img_gcs(prgs.bucket_name)
         blob = buckt.blob(prgs.img_name)
         blob.upload_from_string(img_file.read(), content_type='image/jpg')
 
